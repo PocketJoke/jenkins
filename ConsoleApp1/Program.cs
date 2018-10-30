10b3c89d8dd4211414a4680f69ae19cc34e86e4c
Unknown <chaves.jordan67@gmail.com>
2018-10-30 15:15:11 +0100

    ERROR
    ERROR

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,7 +15,7 @@ namespace Majordome
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+            Console.ReadKey()
         }
     }
 }
