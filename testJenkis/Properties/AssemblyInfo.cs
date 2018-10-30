12508906741ba5b3568f6f0b4d0a9ed5687a75a7
Unknown <chaves.jordan67@gmail.com>
2018-10-30 16:22:46 +0100

    Test
    TEST

---
+++ testJenkis/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("testJenkis")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("testJenkis")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("206b82b7-dd4e-4db2-b6cb-7968c361b96b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ testJenkis/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ConsoleApp1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace testJenkis
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Calcul calc = new Calcul();
+
+            int result = calc.addition(1, 2);
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
